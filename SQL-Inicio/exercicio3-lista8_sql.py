c5eb94ffbbd3e12efdcd1ca0ab827c425057aabf
Thiago de Oliveira <45471767+ThiagoOSantos@users.noreply.github.com>
2025-05-09 17:43:40 -0300

    Update exercicio3-lista8_sql.py
    finalização da 3º questão mas lembrar de ver se tá rodando certo

--- SQL-Inicio/exercicio3-lista8_sql.py
+++ SQL-Inicio/exercicio3-lista8_sql.py
@@ -23,15 +23,38 @@ class ProfessoresDao(ProfessorDao):
         
     def inserir_professor(self, nome, disciplina):
         self.cursor.execute('''
-            INSER IN Professores (nome, disciplina) VALUES (?, ?)
+            INSERT IN Professores (nome, disciplina) VALUES (?, ?)
                             
     
     ''', (nome, disciplina))
         
     def listar_professores(self):
-        self.cursor.execute('SELECT FROM * Professores')
+        self.cursor.execute('SELECT * FROM Professores')
         professores = self.cursor.fetchall()
         print("Lista de Professores")
         for professor in professores:
-            print(f"ID: {professor[0]} Nome: {professor[1]} e Disciplina{professor[2]}")
-        
+            print(f"Encontrado: ID = {professor[0]} Nome: {professor[1]} e Disciplina{professor[2]}")
+
+def menu():
+    dao=ProfessoresDao
+    dao.criar_tabela()
+
+    while True:
+        print("Digite 1 para cadastrar professor e disciplina")
+        print("Digite 2 para listar professor e disciplina")
+        print("Digite 3 para sair")
+        opcao = input("Digite uma opção:\n")
+        if opcao == "1":
+            nome = input("Nome do Professor:\n")
+            ministra = input("Disciplina:\n")
+            dao.inserir_professor()
+            nome2 = input("Nome do próximo Professor")
+            ministra2 = input("Nome da próxima disciplina")
+            dao.inserir_professor()
+        elif opcao == "2":
+            dao.listar_professores()
+        elif opcao == "3":
+            dao.fechar()
+            break
+        else:
+            print("Opção inválida!")
